c26edfa9ae8afc76b0609e4b846f05db51de2dbb
Zeeshan Noorullah <37160247+neptec@users.noreply.github.com>
2023-04-18 12:24:03 +1000

    Add files via upload

---
+++ L1 Demo/L1_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  L1_DemoApp.swift
+//  L1 Demo
+//
+//  Created by Zeeshan Noorullah on 14/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct L1_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
